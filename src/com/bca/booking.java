4411af70f098d29784011b69dd80ac48fddaeaa6
Abhishek AN <darkabhi1520@gmail.com>
2019-10-11 02:07:07 +0530

    booking: Add Edit records capability
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- src/com/bca/booking.java
+++ src/com/bca/booking.java
@@ -98,9 +98,6 @@ public class booking {
         }
     }
 
-    private void scrollPane1MouseClicked(MouseEvent e) {
-        // TODO add your code here
-    }
 
     private void table1MouseClicked(MouseEvent e) {
         DefaultTableModel DFTM = (DefaultTableModel)table1.getModel();
@@ -110,6 +107,53 @@ public class booking {
         textlno.setText(DFTM.getValueAt(selectedIndex,2).toString());
         textdob.setText(DFTM.getValueAt(selectedIndex,3).toString());
         textaddr.setText(DFTM.getValueAt(selectedIndex,4).toString());
+    }
+
+    private void button3ActionPerformed(ActionEvent e) {
+        DefaultTableModel DFTM = (DefaultTableModel)table1.getModel();
+        int selectedIndex = table1.getSelectedRow();
+
+        try {
+            int id = Integer.parseInt(DFTM.getValueAt(selectedIndex,0).toString());
+            String offendername = textname.getText();
+            String licensenumber = textlno.getText();
+            String dob = textdob.getText();
+            String address = textaddr.getText();
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String userName = "abhi";
+            String password = "abhi123";
+            String url = "jdbc:MySQL://localhost/trafficoffenders";
+            connect = DriverManager.getConnection (url, userName, password);
+
+            insert = connect.prepareStatement("update booking set offendername=?,licensenumber=?,dob=?,address=? where id=?");
+            insert.setString(1,offendername);
+            insert.setString(2,licensenumber);
+            insert.setString(3,dob);
+            insert.setString(4,address);
+            insert.setInt(5,id);
+
+
+            insert.executeUpdate();
+
+            JOptionPane.showMessageDialog(abcpanel,"Record Updated");
+            table_update();
+
+            textname.setText("");
+            textlno.setText("");
+            textdob.setText("");
+            textaddr.setText("");
+            textname.requestFocus();
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void scrollPane1MouseClicked(MouseEvent e) {
+        // TODO add your code here
     }
 
     private void initComponents() {
@@ -134,12 +178,13 @@ public class booking {
 
         //======== abcpanel ========
         {
-            abcpanel.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border.
-            EmptyBorder( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER, javax. swing
-            . border. TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 ),
-            java. awt. Color. red) ,abcpanel. getBorder( )) ); abcpanel. addPropertyChangeListener (new java. beans. PropertyChangeListener( )
-            { @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order" .equals (e .getPropertyName () ))
-            throw new RuntimeException( ); }} );
+            abcpanel.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new
+            javax.swing.border.EmptyBorder(0,0,0,0), "JFor\u006dDesi\u0067ner \u0045valu\u0061tion",javax
+            .swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java
+            .awt.Font("Dia\u006cog",java.awt.Font.BOLD,12),java.awt
+            .Color.red),abcpanel. getBorder()));abcpanel. addPropertyChangeListener(new java.beans.
+            PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("bord\u0065r".
+            equals(e.getPropertyName()))throw new RuntimeException();}});
 
             //---- bookingtitle ----
             bookingtitle.setText("Offender Booking");
@@ -168,6 +213,7 @@ public class booking {
 
                 //---- button3 ----
                 button3.setText("Edit");
+                button3.addActionListener(e -> button3ActionPerformed(e));
 
                 //---- label4 ----
                 label4.setText("Address");
